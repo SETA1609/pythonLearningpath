6cd5b98ba91db335a50386955db5b56de0e6d206
seta1609 <sebastian.stp16@gmail.com>
2024-02-02 17:20:45 +0100

    cheatSheet for data types "native/simple"

---
+++ coltSteel/dataTypes/cheatSheet.py
@@ -0,0 +1,14 @@
+#boolean
+is_active = True
+is_inactive = False
+# int whole values
+i = 15
+# float decimal values
+f = 1.0
+# str - String 
+name = "Sebastian"
+# list in other languages array
+first_numbers=[1,2,3]
+# dict in other languages maps
+json = {"key":"value"}
+
