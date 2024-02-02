44c5f501f26141001b9c6b453eed41e00e419154
seta1609 <sebastian.stp16@gmail.com>
2024-02-02 13:14:55 +0100

    typos in numbers were corrected

--- coltSteel/first-python/numbers.py
+++ coltSteel/first-python/numbers.py
@@ -1,14 +1,14 @@
 # addition +
 print("The addition in python is made with + ", 1+1)
 # subtraction -
-print("Substraction is made with - ",2-1)
+print("Subtraction is made with - ",2-1)
 # multiplication *
 print("Multiplication is made with * ", 2*2)
 # division / (float)
 print("division is made with / and always throws a float", 5/2)
-# division // (interger)
-print("if you use // instead of / you recieve a interger as a result ", 5/2)
+# division // (integer)
+print("if you use // instead of / you receive a integer as a result ", 5/2)
 # power **
-print("using ** you elevate the number to the power expresed ", 2**5)
+print("using ** you elevate the number to the power expressed ", 2**5)
 # mod %
 print("the mod operation give you the rest of the division ", 5%2)
